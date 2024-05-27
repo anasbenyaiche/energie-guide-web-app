72856783bf02871bc0b0c8f0d736a95e9d2222bf
anasbenyaiche <anas.benyaiche@yahoo.fr>
2024-05-27 12:02:58 +0100

    let's do it

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="text-3xl font-bold underline"></div>
+      <div className="text-3xl font-bold underline">
+        <h1>Let's do it guys</h1>
+        Vercel{" "}
+      </div>
     </>
   );
 }
