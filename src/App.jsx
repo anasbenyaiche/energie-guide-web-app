295c4b7656f2dc79af3788ab280a513b8393e0e6
anasbenyaiche <anas.benyaiche@yahoo.fr>
2024-06-01 12:34:23 +0100

    handling routing to created pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Login from "./containers/Login";
 import Dashboard from "./containers/Dashboard";
 import { AuthContextProvider } from "./context/AuthContext";
 import ProtectedRoute from "./routes/ProtectedRoutes";
-import Home from "./containers/Home";
 import CreateContent from "./Pages/CreateContent";
 import PageForm from "./containers/PageForm";
 import PreviewContent from "./Pages/PreviewContent";
@@ -11,29 +10,40 @@ import EditPage from "./containers/EditPage";
 import Pages from "./Pages/Pages";
 import MenuItemForm from "./containers/MenuForm";
 import MenuItems from "./Pages/MenuItem";
+import AppContainer from "./containers/AppContainer";
+import { PagesProvider } from "./context/PageContext";
+import Home from "./containers/Home";
 
 const App = () => {
   const isInitiallyAuthenticated = localStorage.getItem("token") !== null; // Example check
 
   return (
-    <AuthContextProvider value={{ isAuthenticated: isInitiallyAuthenticated }}>
-      <Routes>
-        <Route path="/*" element={<Home />} />
-        <Route path="/admin" element={<Login />} />
-        <Route exact path="/" element={<ProtectedRoute />}>
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/pages" element={<Pages />} />
-          <Route path="/admin/pages/create" element={<PageForm />} />
-          <Route path="/admin/edit-page/:id" element={<EditPage />} />
-          <Route path="admin/block/create" element={<CreateContent />} />
-          <Route path="admin/blocks" element={<PreviewContent />} />
-          <Route path="/admin/menu-item" element={<MenuItems />} />
-          <Route path="/admin/menu-item/create" element={<MenuItemForm />} />
-          {/* TODO : still working on */}
-          <Route path="/admin/edit-menu-item/:id" element={<MenuItemForm />} />
-        </Route>
-      </Routes>
-    </AuthContextProvider>
+    <PagesProvider>
+      <AuthContextProvider
+        value={{ isAuthenticated: isInitiallyAuthenticated }}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:link" element={<AppContainer />} />
+          <Route path="/admin" element={<Login />} />
+          <Route exact path="/" element={<ProtectedRoute />}>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/pages" element={<Pages />} />
+            <Route path="/admin/pages/create" element={<PageForm />} />
+            <Route path="/admin/edit-page/:id" element={<EditPage />} />
+            <Route path="admin/block/create" element={<CreateContent />} />
+            <Route path="admin/blocks" element={<PreviewContent />} />
+            <Route path="/admin/menu-item" element={<MenuItems />} />
+            <Route path="/admin/menu-item/create" element={<MenuItemForm />} />
+            {/* TODO : still working on */}
+            <Route
+              path="/admin/edit-menu-item/:id"
+              element={<MenuItemForm />}
+            />
+          </Route>
+        </Routes>
+      </AuthContextProvider>
+    </PagesProvider>
   );
 };
 
